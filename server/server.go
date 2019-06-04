45d1e0f530e8a75a6b72086bd1cf08730785b5e4
shynome <shynome@gmail.com>
2019-06-04 14:24:32 +0800

    不需要写那么长

--- server/server.go
+++ server/server.go
@@ -11,10 +11,6 @@ import (
 var APIMux = http.NewServeMux()
 
 func init() {
-	APIMux.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
-		user.APIMux.ServeHTTP(w, r)
-	})
-	APIMux.HandleFunc("/ldap/", func(w http.ResponseWriter, r *http.Request) {
-		ldap.APIMux.ServeHTTP(w, r)
-	})
+	APIMux.Handle("/user/", user.APIMux)
+	APIMux.Handle("/ldap/", ldap.APIMux)
 }
